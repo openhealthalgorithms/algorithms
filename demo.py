b683d3a5a0479b821d5396692636c5cc78f10ff1
indrajit <eendroroy@gmail.com>
2017-01-27 01:00:01 +0600

    update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 from OpenHealthAlgorithms.Diabetes import Diabetes
+from OpenHealthAlgorithms.Framingham import Framingham
 
 params = {
     'gender':    'M',
@@ -11,4 +12,17 @@ params = {
     'hip':       90,
 }
 result = Diabetes().calculate(params)
-print(result)
+print '{0: <20}'.format(' --> Diabetes:'), result
+
+params = {
+   'gender':            'F',
+   'age':               40,
+   'total_cholesterol': 180,
+   'hdl_cholesterol':   45,
+   'systolic':          125,
+   'on_bp_medication':  False,
+   'is_smoker':         False,
+   'has_diabetes':      False,
+}
+result = Framingham().calculate(params)
+print '{0: <20}'.format(' --> Framingham:'), result
