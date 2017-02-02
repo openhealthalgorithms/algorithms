0fb6f5a159f5493c4fe83591d9a62f0f719f3fcc
indrajit <eendroroy@gmail.com>
2017-02-03 00:17:48 +0600

    added package requirements

--- setup.py
+++ setup.py
@@ -31,6 +31,9 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
     packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ]
     # entry_points={
     #     'console_scripts': [
     #         'spike=spikes:main',
