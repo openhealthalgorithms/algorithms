69af19622fa44c653e7855a0bdb9c0d5a5699800
indrajit <eendroroy@gmail.com>
2017-08-20 21:43:21 +0600

    cholesterol unit converter

--- OpenHealthAlgorithms/__helpers.py
+++ OpenHealthAlgorithms/__helpers.py
@@ -3,3 +3,14 @@ def format_params(params):
         key: float(value) if type(value) is int else value
         for key, value in params.items()
     }
+
+def convert_cholesterol_unit(value, from_unit, to_unit):
+    if from_unit == to_unit:
+        return value
+    elif to_unit.lower() == 'mmol/l':
+        return (value * 0.02586)
+    elif to_unit.lower() == 'mg/dl':
+        return value * 88.57
+    else:
+        return None
+
