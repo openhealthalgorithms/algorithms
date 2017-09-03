475e71b2b8517170cdc684088e1c8a76073451bc
indrajit <eendroroy@gmail.com>
2017-09-04 05:12:42 +0600

    WHO param builder

---
+++ OHA/param_builders/who_param_builder.py
@@ -0,0 +1,56 @@
+class WhoParamsBuilder(object):
+    def __init__(self):
+        self.__gender = "M"
+        self.__age = 40
+        self.__sbp1 = 140
+        self.__sbp2 = 160
+        self.__chol = 5
+        self.__chol_unit = 'mmol/l'
+        self.__smoker = False
+        self.__diabetic = False
+
+    def gender(self, gender):
+        self.__gender = gender
+        return self
+
+    def age(self, age):
+        self.__age = age
+        return self
+
+    def sbp1(self, sbp1):
+        self.__sbp1 = sbp1
+        return self
+
+    def sbp2(self, sbp2):
+        self.__sbp2 = sbp2
+        return self
+
+    def chol(self, chol):
+        self.__chol = chol
+        return self
+
+    def chol_unit(self, chol_unit):
+        self.__chol_unit = chol_unit
+        return self
+
+    def smoker(self, smoker=True):
+        self.__smoker = smoker
+        return self
+
+    def diabetic(self, diabetic=True):
+        self.__diabetic = diabetic
+        return self
+
+    def build(self):
+        params = {
+            'gender': self.__gender,
+            'age': self.__age,
+            'systolic_blood_pressure_1': self.__sbp1,
+            'systolic_blood_pressure_2': self.__sbp2,
+            'cholesterol': self.__chol,
+            'cholesterol_unit': self.__chol_unit,
+            'is_smoker': self.__smoker,
+            'has_diabetes': self.__diabetic,
+        }
+
+        return params
