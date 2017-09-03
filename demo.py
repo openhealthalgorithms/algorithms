e85b28a24a0a994920d5fccfebf2a9dde80af462
indrajit <eendroroy@gmail.com>
2017-09-04 05:51:01 +0600

    update demo

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 from OHA.Diabetes import Diabetes
 from OHA.Framingham import Framingham
 from OHA.WHO import WHO
-from OHA.param_builders.who_param_builder import WhoParamsBuilder
+from OHA.param_builders.who_param_builder import WhoParamsBuilder as WPB
 
 params = {
     'gender': 'M',
@@ -35,16 +35,18 @@ params = {
 result = Framingham().calculate(params)
 print('--> Framingham:', result)
 
-WPB = WhoParamsBuilder()
-
-params = WPB.gender("M").age(50).sbp1(150).sbp2(170).chol(7, 'mmol/l').region('SEARD').smoker().diabetic().build()
+params = WPB().gender("M").age(50).sbp1(150).sbp2(170).chol(7, 'mmol/l').region('SEARD').smoker().diabetic().build()
 result = WHO().calculate(params)
 print('--> WHO:', params['region'], ' => ',  result)
 
-params = WPB.gender("M").age(50).sbp1(150).sbp2(170).chol(7, 'mmol/l').region('AFRD').smoker().diabetic().build()
+params = WPB().gender("M").age(50).sbp1(150).sbp2(170).chol(7, 'mmol/l').region('AFRD').smoker().diabetic().build()
 result = WHO().calculate(params)
 print('--> WHO:', params['region'], ' => ',  result)
 
-params = WPB.gender("M").age(50).sbp1(150).sbp2(170).chol(7, 'mmol/l').region('AFRE').smoker().diabetic().build()
+params = WPB().gender("M").age(50).sbp1(150).sbp2(170).chol(7, 'mmol/l').region('AFRE').smoker().diabetic().build()
+result = WHO().calculate(params)
+print('--> WHO:', params['region'], ' => ',  result)
+
+params = WPB().gender("M").age(70).sbp1(130).sbp2(145).chol(270, 'mg/dl').smoker().diabetic().build()
 result = WHO().calculate(params)
 print('--> WHO:', params['region'], ' => ',  result)
