dbca54c3c9f5a5f9666482e89b748b09bda468c8
indrajit <eendroroy@gmail.com>
2017-09-04 05:50:47 +0600

    update tests

--- tests/test_who.py
+++ tests/test_who.py
@@ -4,95 +4,41 @@
 import unittest
 
 from OHA.WHO import WHO
+from OHA.param_builders.who_param_builder import WhoParamsBuilder as WPB
 
 
-class FraminghamTest(unittest.TestCase):
+class WhoTest(unittest.TestCase):
+
     def test_should_produce_exception(self):
-        params = {
-            'gender': "M",
-            'age': 18,
-            'systolic_blood_pressure_1': 130,
-            'systolic_blood_pressure_2': 145,
-            'cholesterol': 5,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = WPB().gender("M").age(18).sbp1(130).sbp2(145).chol(5).smoker().diabetic().build()
         result = WHO().calculate(params)
-        self.assertEqual(result['exception'], 'file not found')
+        self.assertEqual(result['exception'], 'color chart not found.')
 
-        params = {
-            'gender': "M",
-            'age': 100,
-            'systolic_blood_pressure_1': 130,
-            'systolic_blood_pressure_2': 145,
-            'cholesterol': 5,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = WPB().gender("M").age(100).sbp1(130).sbp2(145).chol(5).smoker().diabetic().build()
         result = WHO().calculate(params)
-        self.assertEqual(result['exception'], 'file not found')
+        self.assertEqual(result['exception'], 'color chart not found.')
 
     def test_risk_is_10(self):
-        params = {
-            'gender': "M",
-            'age': 60,
-            'systolic_blood_pressure_1': 130,
-            'systolic_blood_pressure_2': 145,
-            'cholesterol': 4,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = WPB().gender("M").age(60).sbp1(130).sbp2(145).chol(4).smoker().diabetic().build()
         result = WHO().calculate(params)
         self.assertEqual(result['risk'], 10)
 
     def test_risk_is_20(self):
-        params = {
-            'gender': "M",
-            'age': 40,
-            'systolic_blood_pressure_1': 130,
-            'systolic_blood_pressure_2': 145,
-            'cholesterol': 7,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = WPB().gender("M").age(40).sbp1(130).sbp2(145).chol(7).smoker().diabetic().build()
         result = WHO().calculate(params)
         self.assertEqual(result['risk'], 20)
 
     def test_risk_is_30(self):
-        params = {
-            'gender': "M",
-            'age': 60,
-            'systolic_blood_pressure_1': 180,
-            'systolic_blood_pressure_2': 185,
-            'cholesterol': 4,
-            'is_smoker': False,
-            'has_diabetes': False,
-        }
+        params = WPB().gender("M").age(60).sbp1(180).sbp2(185).chol(4).build()
         result = WHO().calculate(params)
         self.assertEqual(result['risk'], 30)
 
     def test_risk_is_40(self):
-        params = {
-            'gender': "M",
-            'age': 70,
-            'systolic_blood_pressure_1': 140,
-            'systolic_blood_pressure_2': 150,
-            'cholesterol': 5,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = WPB().gender("M").age(70).sbp1(140).sbp2(150).chol(5).smoker().diabetic().build()
         result = WHO().calculate(params)
         self.assertEqual(result['risk'], 40)
 
     def test_risk_is_50(self):
-        params = {
-            'gender': "F",
-            'age': 80,
-            'systolic_blood_pressure_1': 160,
-            'systolic_blood_pressure_2': 180,
-            'cholesterol': 7,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = WPB().gender("F").age(80).sbp1(160).sbp2(180).chol(7).smoker().diabetic().build()
         result = WHO().calculate(params)
         self.assertEqual(result['risk'], 50)
