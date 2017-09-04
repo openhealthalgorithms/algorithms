72cf7e30a3a63daae5458799fc062a05d4995464
indrajit <eendroroy@gmail.com>
2017-09-04 06:17:12 +0600

    update tests

--- tests/test_framingham.py
+++ tests/test_framingham.py
@@ -4,47 +4,21 @@
 import unittest
 
 from OHA.Framingham import Framingham
+from OHA.param_builders.framingham_param_builder import FraminghamParamsBuilder as FPB
 
 
 class FraminghamTest(unittest.TestCase):
     def test_heart_age_and_actual_age_is_same(self):
-        params = {
-            'gender': 'F',
-            'age': 40,
-            'total_cholesterol': 180,
-            'hdl_cholesterol': 45,
-            'systolic': 125,
-            'on_bp_medication': False,
-            'is_smoker': False,
-            'has_diabetes': False,
-        }
+        params = FPB().gender("F").age(40).t_chol(180).hdl_chol(45).sbp(125).build()
         result = Framingham().calculate(params)
         self.assertEqual(result['heart_age'], params['age'])
 
     def test_heart_age_greater_than_actual_age(self):
-        params = {
-            'gender': 'F',
-            'age': 40,
-            'total_cholesterol': 280,
-            'hdl_cholesterol': 45,
-            'systolic': 125,
-            'on_bp_medication': True,
-            'is_smoker': True,
-            'has_diabetes': True,
-        }
+        params = FPB().gender("F").age(40).t_chol(280).hdl_chol(45).sbp(125).bp_medication().smoker().diabetic().build()
         result = Framingham().calculate(params)
         self.assertGreater(result['heart_age'], params['age'])
 
     def test_heart_age_less_than_actual_age(self):
-        params = {
-            'gender': 'F',
-            'age': 40,
-            'total_cholesterol': 100,
-            'hdl_cholesterol': 30,
-            'systolic': 125,
-            'on_bp_medication': False,
-            'is_smoker': False,
-            'has_diabetes': False,
-        }
+        params = FPB().gender("F").age(40).t_chol(100).hdl_chol(30).sbp(125).build()
         result = Framingham().calculate(params)
         self.assertLess(result['heart_age'], params['age'])
