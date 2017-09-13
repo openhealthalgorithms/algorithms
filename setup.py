f120e872c511d66e2eb293cdbb42dc9025dce7ad
indrajit <eendroroy@gmail.com>
2017-09-13 21:19:14 +0600

    v0.1.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def find_packages(*args, **kwargs):
 
 setup(
     name='OpenHealthAlgorithms',
-    version='0.1.6',
+    version='0.1.7',
     description='An implementation of health algorithms.',
     long_description=long_description,
     url='https://github.com/openhealthalgorithms/openhealthalgorithms',
