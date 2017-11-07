acc7ff755970f120cc6d6462f063a08249406fe7
indrajit <eendroroy@gmail.com>
2017-11-07 12:39:27 +0600

    defaults

---
+++ OHA/Defaults.py
@@ -0,0 +1,7 @@
+class Defaults(object):
+    default_weight_unit = 'kg'
+    default_height_unit = 'm'
+    default_waist_unit = 'cm'
+    default_hip_unit = 'cm'
+    default_cholesterol_unit = 'mmol/l'
+
