aa377f07b898171fd9038fb588834a8acae66477
indrajit <eendroroy@gmail.com>
2018-01-17 17:40:04 +0600

    PhysicalActivityAssessment

---
+++ OHA/assessments/PhysicalActivityAssessment.py
@@ -0,0 +1,25 @@
+from OHA.assessments.Assessment import Assessment
+
+__author__ = 'indrajit'
+__email__ = 'eendroroy@gmail.com'
+
+
+class PhysicalActivityAssessment(Assessment):
+    def __init__(self, input_data=None):
+        super().__init__(input_data)
+
+    def __active_time(self):
+        return self._get_data()['active_time']
+
+    def __targets_active_time(self):
+        return self._get_data()['targets_active_time']
+
+    def assess(self):
+        target = "150 minutes"
+
+        if int(self.__active_time()) >= self.__targets_active_time():
+            result_code = "PA-1"
+        else:
+            result_code = "PA-2"
+
+        return dict(value=self.__active_time(), code=result_code, target=target)
