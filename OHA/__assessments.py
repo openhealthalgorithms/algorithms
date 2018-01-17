09241e496e39f1f37e5c1491fb7796a856c309e2
indrajit <eendroroy@gmail.com>
2018-01-18 01:47:14 +0600

    refactor

--- OHA/__assessments.py
+++ OHA/__assessments.py
@@ -1,26 +1,26 @@
 def assess_waist_hip_ratio(waist, hip, gender):
-    raise NotImplementedError('"assess_waist_hip_ratio" method removed')
+    raise NotImplementedError('Use "WHRAssessment"')
 
 
 def assess_smoking_status(smoking):
-    raise NotImplementedError('"assess_smoking_status" method removed')
+    raise NotImplementedError('Use "SmokingAssessment"')
 
 
 def assess_blood_pressure(bp, conditions):
-    raise NotImplementedError('"assess_blood_pressure" method removed')
+    raise NotImplementedError('Use "BPAssessment"')
 
 
 def assess_bmi(bmi):
-    raise NotImplementedError('"assess_bmi" method removed')
+    raise NotImplementedError('Use "BMIAssessment"')
 
 
 def assess_diet(diet_history, conditions, targets):
-    raise NotImplementedError('"assess_diet" method removed')
+    raise NotImplementedError('Use "DietAssessment"')
 
 
 def assess_physical_activity(active_time, targets):
-    raise NotImplementedError('"assess_physical_activity" method removed')
+    raise NotImplementedError('Use "PhysicalActivityAssessment"')
 
 
 def calculate_diabetes_status(conditions, bsl_type, bsl_units, bsl_value):
-    raise NotImplementedError('"calculate_diabetes_status" method removed')
+    raise NotImplementedError('Use "DiabetesAssessment"')
