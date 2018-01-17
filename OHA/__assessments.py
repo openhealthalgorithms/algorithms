8882213bf7f69654b3c35b77ad1515eff8948e3b
indrajit <eendroroy@gmail.com>
2018-01-18 01:42:25 +0600

    remove assess_diet method

--- OHA/__assessments.py
+++ OHA/__assessments.py
@@ -15,48 +15,7 @@ def assess_bmi(bmi):
 
 
 def assess_diet(diet_history, conditions, targets):
-    """
-    General Rules:
-    - Aim for 50% fruit & vegetables (with specific targets for F&V), 25% lean protein, 25% carbohydrates
-    - Minimise amount of fast foods, processed foods
-    - Replace soda with non-sugary alternatives
-    - Limit amount of added salt & sugar - for diabetics and hypertensives, target is 0
-    - If trying to lose weight, avoid hotel or fast food (not sure what is in them)
-    - For diabetes:
-        - no added sugar
-        - If BMI > 25, aim for weight loss of 600-700 calories per day
-    - For Hypertensive:
-        - no added salt
-    Step 1: Assess general diet, fruits & vegetables based on targets (2 fruits & 5 vegetables per day)
-    Step 2: Proportion of carbohydrates - should be 25% (other 25% lean protein, 50% vegetables, salads)
-    Step 3: Amount of soda or other added sugars
-    """
-
-    if diet_history['fruit'] < targets['general']['diet']['fruit'] \
-            and diet_history['veg'] < targets['general']['diet']['vegetables']:
-        # Both F&V off target
-        result_code = 'NUT-3'
-    elif ((diet_history['fruit'] < targets['general']['diet']['fruit'])
-          and (diet_history['veg'] >= targets['general']['diet']['vegetables'])):
-        # Partial F&V off target
-        result_code = 'NUT-2'
-    elif ((diet_history['fruit'] > targets['general']['diet']['fruit'])
-          and (diet_history['veg'] < targets['general']['diet']['vegetables'])):
-        # Partial F&V off target
-        result_code = 'NUT-2'
-    else:
-        # Partial F&V ON target
-        result_code = 'NUT-1'
-
-    diet_output = {
-        'values': {
-            'fruit': diet_history['fruit'],
-            'vegetables': diet_history['veg']
-        },
-        'code': result_code
-    }
-
-    return diet_output
+    raise NotImplementedError('"assess_diet" method removed')
 
 
 def assess_physical_activity(active_time, targets):
