f0b2a791b8a06b2e6612607d6b9750575c094918
Fred Hersch <fredhersch@gmail.com>
2018-01-19 18:35:39 +0800

    Added test cases

--- tests.py
+++ tests.py
@@ -1,39 +1,44 @@
 import json
 import pandas as pd
 
-from OHA.Diabetes import Diabetes
-from OHA.Framingham import Framingham
-from OHA.HEARTS import HEARTS
-from OHA.HealthAssessment import HealthAssessment as HA
+#from OHA.Diabetes import Diabetes
+#from OHA.Framingham import Framingham
+#from OHA.HEARTS import HEARTS
+#from OHA.HealthAssessment import HealthAssessment as HA
 from OHA.WHO import WHO
-from OHA.param_builders.diabetes_param_builder import DiabetesParamsBuilder as DBP
-from OHA.param_builders.framingham_param_builder import FraminghamParamsBuilder as FPB
+#from OHA.param_builders.diabetes_param_builder import DiabetesParamsBuilder as DBP
+#from OHA.param_builders.framingham_param_builder import FraminghamParamsBuilder as FPB
 from OHA.param_builders.who_param_builder import WhoParamsBuilder as WPB
 
+def calculate_hearts_risk(row):
+	# column names: country,age,gender,bp,total_chol,smoker,diabetes,cvd_risk
+	is_smoker = False
+	has_diabetes = False
+	if row['smoker'] == '1':
+		is_smoker = True
+
+	if row['diabetes'] == '1':
+		has_diabetes = True
+
+	params = WPB().gender(row['gender']).age(row['age']).sbp1(row['bp']).sbp2(row['bp']).chol(row['total_chol'], 'mmol/l').region(row['country']).smoker(is_smoker).diabetic(has_diabetes).build()
+	result = WHO().calculate(params)
+	#print('--> WHO:', params, ' => ', result, '---\n')
+	return result['risk_range']
+
+print('---> running tests ---')
+
 filename = 'OHA/tests/who_tests.csv'
-tests = pd.read_csv(filename)
-print(tests.shape)
-print(tests.head())
+tests_df = pd.read_csv(filename)
+#print(tests_df.shape)
+#print(tests_df.head())
 
-print('\n --- Running Tests ---')
-params = WPB().gender("M").age(43).sbp1(150).sbp2(140).chol(4.1, 'mmol/l').region('AFRE').smoker().diabetic(True).build()
-result = WHO().calculate(params)
-print('--> WHO:', params, ' => ', result)
+tests_df['calculated'] = tests_df.apply(calculate_hearts_risk, axis=1)
+tests_df['result'] = tests_df.apply(lambda x: 'equal' if x['cvd_risk'] == x['calculated'] else 'error', axis=1)
 
-params = WPB().gender("M").age(43).sbp1(180).sbp2(180).chol(5.1, 'mmol/l').region('AFRD').smoker().diabetic(True).build()
-result = WHO().calculate(params)
-print('--> WHO:', params, ' => ', result)
+# add the date-time to the filname
+filename = 'OHA/tests/who_tests_processed.csv'
+print('---> completed tests ---')
+tests_df.to_csv(filename)
 
-params = WPB().gender("F").age(52).sbp1(165).sbp2(166).chol(6.1, 'mmol/l').region('AFRD').smoker(True).diabetic(False).build()
-result = WHO().calculate(params)
-print('--> WHO:', params, ' => ', result)
 
-print('\n --- Running Tests ---')
-params = WPB().gender("M").age(43).sbp1(150).sbp2(140).chol(4.1, 'mmol/l').region('AFRD').smoker().diabetic(True).build()
-result = WHO().calculate(params)
-print('--> WHO:', params, ' => ', result)
 
-print('\n --- Running Tests ---')
-params = WPB().gender("F").age(40).sbp1(169).sbp2(169).chol(5.2, 'mmol/l').region('AFRD').smoker(True).diabetic(False).build()
-result = WHO().calculate(params)
-print('--> WHO:', params, ' => ', result)
