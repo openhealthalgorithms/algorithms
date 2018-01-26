edc11baaa2221147c293cc85045c5eba8d40b35c
indrajit <eendroroy@gmail.com>
2018-01-27 02:16:41 +0600

    remove deprecated files

--- OHA/__helpers.py
+++
@@ -1,6 +0,0 @@
-def format_params(params):
-    raise NotImplementedError('Use "ParamFormatter"')
-
-
-def find_age_index(age, age_brackets):
-    raise NotImplementedError('method "find_age_index" removed.')
