e494025e0eb22f2a742321abb7d3e614e50b59c4
indrajit <eendroroy@gmail.com>
2018-02-09 22:10:41 +0600

    remove convert_cholesterol_unit method

--- OHA/__unit.py
+++ OHA/__unit.py
@@ -1,12 +1,5 @@
 def convert_cholesterol_unit(value, from_unit, to_unit):
-    if from_unit.lower() == to_unit.lower():
-        return value
-    elif to_unit.lower() == 'mmol/l':
-        return value * 0.02586
-    elif to_unit.lower() == 'mg/dl':
-        return value * 88.57
-    else:
-        return None
+    raise NotImplementedError('use "helpers.converter.CholesterolConverter"')
 
 
 def convert_weight_unit(value, from_unit, to_unit):
